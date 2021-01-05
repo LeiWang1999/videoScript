ea87e6ac2465c674d5b6e3b0797fbdad23b98f28
LeiWan1999 <2230647190@qq.com>
2021-01-05 12:44:12 +0800

    change mkv_ch2mp4 bitrate

--- mkv_ch2mp4.sh
+++ mkv_ch2mp4.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 #mkv视频转mp4
+bitrate=$1
 filelist=$(find *.mkv)
-OLDIFS="$IFS"
-IFS=$"\n"
+if [ ! $bitrate ]; then 
 for filename in *.mkv
 do
-	    ffmpeg -i $filename -c:v libx264 -c:a aac -strict -2 "${filename%.*}".mp4
-    done
-    IFS=$OLDIFS
-
+	ffmpeg -i $filename -c:v libx264 -c:a aac -strict -2 "${filename%.*}".mp4
+done
+else
+for filename in *.mkv
+do
+	ffmpeg -i $filename -c:v libx264 -c:a aac -strict -2 -b 1500k "${filename%.*}".mp4
+done
+fi
--- mkv_ch2mp4_1500.sh
+++ mkv_ch2mp4_1500.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-#mkv视频转mp4
+# mkv视频转mp4
+# 可以更换码率和字幕通道
+bitrate=$1
+subtitleChannel=$2
+echo $bitrate
+echo $subtitleChannel
+
 filelist=$(find *.mkv)
-OLDIFS="$IFS"
-IFS=$"\n"
 for filename in *.mkv
 do
 	    ffmpeg -i $filename -c:v libx264 -c:a aac -strict -2 -b 1500k "${filename%.*}".mp4
-    done
-    IFS=$OLDIFS
+done
 
