9951b8b7de389cda506e35b8fa70262c70f2cfeb
LeiWan1999 <2230647190@qq.com>
2021-01-05 12:50:02 +0800

    change mkv_ch2mp4_sub bitrate

--- mkv_ch2mp4_sub.sh
+++ mkv_ch2mp4_sub.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 # mkv视频转mp4带字幕
+bitrate=$1
 filelist=$(find *.mkv)
+if [ ! $bitrate ]; then
+for filename in *.mkv
+do
+ffmpeg -i $filename -b $bitrate -c:v libx264 -c:a aac -vf subtitles=$filename -c:a aac -strict -2 "${filename%.*}".mp4
+done
+else
 for filename in *.mkv
 do
 ffmpeg -i $filename -c:v libx264 -c:a aac -vf subtitles=$filename -c:a aac -strict -2 "${filename%.*}".mp4
 done
-    IFS=$OLDIFS
+fi
 
